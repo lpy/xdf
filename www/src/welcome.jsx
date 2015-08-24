adee146c505ca2c5840b39ad69852b8310cd7c42
jennings <jennings19930517@gmail.com>
2015-08-24 20:11:55 +0800

    首页标题不换行

--- www/src/welcome.jsx
+++ www/src/welcome.jsx
@@ -8,9 +8,9 @@ var Welcome = React.createClass({
 			<div className="welcomePage">
 				<div className="welcome">
 					<img src="images/title.png"></img>
-					<h2>({assignment.name})</h2>
+					
 				</div>
-				
+				<h2>({assignment.name})</h2>
 				<a href="#/quiz/0"><img className="quizBtn" src="images/quizBtn.png" /></a>
 				<img src="images/logo.png" className="logo"></img>
 			</div>
